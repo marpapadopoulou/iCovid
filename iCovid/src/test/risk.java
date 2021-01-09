3e8d7cbbbb194fff8dc489162c7a1cbd4720f92b
Μαρία Παπαδοπούλου < @192.168.153.1>
2021-01-09 21:54:32 +0200

    changed

--- iCovid/src/test/risk.java
+++ iCovid/src/test/risk.java
@@ -7,84 +7,84 @@ public class risk {
 	public static int calculateRisk(result result) {
 		int totalPoints=0;
 		
-		if(result.age.equals("18-34")) {
+		if(result.age=="18-34") {
 			totalPoints=totalPoints + 5;
 		}
-		else if(result.age.equals( "35-50")) {
+		else if(result.age == "35-50") {
 			totalPoints=totalPoints + 6;
 		}
-		else if(result.age.equals("51-65")) {
+		else if(result.age == "51-65") {
 			totalPoints=totalPoints + 8;
 		}
-		else if(result.age.equals( "66-79")) {
+		else if(result.age== "66-79") {
 			totalPoints=totalPoints + 9;
 		}
 		else  {
 			totalPoints=totalPoints + 10;
 		}
 		
-		if(result.temp.equals("35.5°C - 37°C")) {
+		if(result.temp=="35.5°C - 37°C") {
 			totalPoints=totalPoints + 0;
 		}
-		else if(result.temp.equals("37.1°C - 37.9°C")) {
+		else if(result.temp=="37.1°C - 37.9°C") {
 			totalPoints=totalPoints + 6;
 		}
-		else if(result.temp.equals( "38°C - 38.5°C")) {
+		else if(result.temp== "38°C - 38.5°C") {
 			totalPoints=totalPoints + 9;
 		}
 		else  {
 			totalPoints=totalPoints + 10;
 		}
 		
-		if(result.drycough.equals("yes")){
+		if(result.drycough=="yes"){
 			totalPoints=totalPoints + 10;
 		}
 		else {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.breathing.equals("yes")) {
+		if(result.breathing=="yes") {
 			totalPoints=totalPoints + 10;
 		}
 		else {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.smelltaste.equals("yes")) {
+		if(result.smelltaste=="yes") {
 			totalPoints=totalPoints + 10;
 		}
 		else {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.exhausted.equals("yes")) {
+		if(result.exhausted=="yes") {
 			totalPoints=totalPoints + 10;
 		}
 		else {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.question8.equals("sorethroat")) {
+		if(result.question8 == "sorethroat") {
 			totalPoints=totalPoints + 3;
 		}
-		else if(result.question8.equals("headache")) {
+		else if(result.question8=="headache") {
 			totalPoints=totalPoints + 3;
 		}
-		else if(result.question8.equals("diarrhea")) {
+		else if(result.question8 == "diarrhea") {
 			totalPoints=totalPoints + 3;
 		}
 		else  {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.confirmedcase.equals("yes")) {
+		if(result.confirmedcase == "yes") {
 			totalPoints=totalPoints + 10;
 		}
 		else {
 			totalPoints=totalPoints + 0;
 		}
 		
-		if(result.vulnerability.equals("yes")) {
+		if(result.vulnerability == "yes") {
 			totalPoints=totalPoints + 10;
 		}
 		else {
