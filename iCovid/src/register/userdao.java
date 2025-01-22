0d8d20874049ec86f0af47211964c5bf1ea344e4
marpapadopoulou <72757394+marpapadopoulou@users.noreply.github.com>
2025-01-22 12:07:12 +0200

    Update userdao.java

--- iCovid/src/register/userdao.java
+++ iCovid/src/register/userdao.java
@@ -1,5 +1,4 @@
 package register;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,138 +6,96 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
-
 public class userdao {
+    private static final String DB_URL = "jdbc:postgresql://dpg-cu7skld6l47c73frnl20-a.frankfurt-postgres.render.com:5432/icovid_db?ssl=true";
+    private static final String DB_USERNAME = "icovid_db_user";
+    private static final String DB_PASSWORD = "IeCDe94K0MwPo1SLAENBaKSdgkOKDvrI";
+    private static final String DB_DRIVER = "org.postgresql.Driver";
 
-    private String dbUrl = "jdbc:postgresql://dpg-cu7skld6l47c73frnl20-a.frankfurt-postgres.render.com:5432/icovid_db?ssl=true";
-    private String dbUname = "icovid_db_user";
-    private String dbPassword = "IeCDe94K0MwPo1SLAENBaKSdgkOKDvrI"; 
-    private String dbDriver = "org.postgresql.Driver";
+    static {
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError("Failed to load PostgreSQL driver: " + e.getMessage());
+        }
+    }
 
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 
+    public boolean insert(user user) {
+        String sql = "INSERT INTO users (name, email, lat, lng, res) VALUES (?, ?, ?, ?, ?)";
+        
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setFloat(3, user.getLat());
+            ps.setFloat(4, user.getLng());
+            ps.setFloat(5, user.getRes());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.err.println("Error inserting user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	
-	public void loadDriver(String dbDriver){
-		try {
-			Class.forName(dbDriver);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	public Connection getConnection(){
-		Connection con = null ;
-		try {
-			Class.forName(dbDriver);
-			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
-			System.out.println("Database connection established successfully.");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Failed to establish database connection.");
-			e.printStackTrace();
-		}  catch (ClassNotFoundException e) {
-		        System.out.println("PostgreSQL Driver not found.");
-		        e.printStackTrace();
-		}
-		return con;
-	}
-	
-	//for register user
-	public boolean insert(user user)
-	{
-		boolean res=false;
-		loadDriver(dbDriver);
-		Connection con = getConnection();
-		String sql = "INSERT INTO users  (name, email, lat, lng, res) VALUES (?,?,?,?,?)";
-		
-		PreparedStatement ps;
-		try {
-		ps = con.prepareStatement(sql);
-		ps.setString(1, user.getName());
-		ps.setString(2, user.getEmail());
-		ps.setFloat(3, user.getLat());
-		ps.setFloat(4, user.getLng());
-		ps.setFloat(5, user.getRes());
-		ps.executeUpdate();
-		res=true;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return res;
-	}
-	
-	
-	public boolean validate(user user)
-	{
-		boolean status=false;
-		
-		loadDriver(dbDriver);
-		Connection con = getConnection();
-		
-		String sql = "SELECT * FROM users WHERE email= ? ";
-		
-		PreparedStatement ps;
-		try {
-		ps = con.prepareStatement(sql);
-		ps.setString(1,user.getEmail());
-		
-		ResultSet rs = ps.executeQuery();
-		status= rs.next();
-		
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return status;
-	}
-	
-	public  ArrayList<Float> getCoords()
-	{
-		ArrayList<Float> coords=new ArrayList<Float>();
-		loadDriver(dbDriver);
-		Connection con = getConnection();
-		
-		String sql = "SELECT lat,lng,res FROM users";
-		
-		PreparedStatement ps;
-		try {
-			ps = con.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				coords.add(rs.getFloat("lat"));
-				coords.add(rs.getFloat("lng"));
-				coords.add(rs.getFloat("res"));
-			}
-			 //System.out.println(coords);
-		}catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-	}
-	return coords;
-	}
-	
-	public void insertRes(Float res, user user) {
-		
-		loadDriver(dbDriver);
-		Connection con = getConnection();
-		
-		String sql="UPDATE users SET res=? WHERE email=?";
-		
-		PreparedStatement ps;
-		try {
-			ps = con.prepareStatement(sql);
-			ps.setFloat(1, res);
-			ps.setString(2, user.getEmail());
-			ps.executeUpdate();
-		}catch(SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public boolean validate(user user) {
+        String sql = "SELECT 1 FROM users WHERE email = ?";
+        
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            
+            ps.setString(1, user.getEmail());
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            System.err.println("Error validating user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ArrayList<Float> getCoords() {
+        ArrayList<Float> coords = new ArrayList<>();
+        String sql = "SELECT lat, lng, res FROM users";
+        
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            
+            while (rs.next()) {
+                coords.add(rs.getFloat("lat"));
+                coords.add(rs.getFloat("lng"));
+                coords.add(rs.getFloat("res"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Error retrieving coordinates: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return coords;
+    }
+
+    public boolean insertRes(Float res, user user) {
+        String sql = "UPDATE users SET res = ? WHERE email = ?";
+        
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            
+            ps.setFloat(1, res);
+            ps.setString(2, user.getEmail());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.err.println("Error updating user result: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
